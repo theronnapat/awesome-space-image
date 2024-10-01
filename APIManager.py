f1ccefe85eaeb196cc1d3233ece08dcacf5867dc
Vimal Mishra <vimal.mishra@myanatomy.in>
2024-10-02 00:08:50 +0530

    Topic : Improvements required

--- APIManager.py
+++ APIManager.py
@@ -1,32 +1,32 @@
-import json
-import sys
 import requests
+import sys
 
 class NasaAPI:
     def __init__(self, api_key):
         self._api_key = api_key
 
-    def get_api_key(self):
-        return self._api_key
-
     def get_today_image(self, update):
-        f = open("./README.md", "w")
+        try:
+            # API endpoint
+            endpoint = f'https://api.nasa.gov/planetary/apod?api_key={self._api_key}'
 
-        endpoint = 'https://api.nasa.gov/planetary/apod?api_key={}'.format(self._api_key)
+            # Making the request
+            res = requests.get(endpoint)
 
-        res = requests.get(endpoint)
-        data = json.loads(res.text)
+            # Check for request errors
+            if res.status_code != 200:
+                sys.exit(f"Error: Failed to fetch data, status code {res.status_code}.\n")
 
-        if 'date' not in data:
-            sys.exit("Error: date not found in data.\n")
+            # Parse response JSON
+            data = res.json()
 
-        if 'title' not in data:
-            sys.exit("Error: title not found in data.\n")
+            # Validating necessary keys in response data
+            if 'date' not in data or 'title' not in data or 'url' not in data:
+                sys.exit("Error: Required fields (date, title, url) not found in data.\n")
 
-        if 'url' not in data:
-            sys.exit("Error: url not found in data.\n")
-
-        f.write(f"""
+            # Writing to README.md
+            with open("./README.md", "w") as f:
+                f.write(f"""
 # Awesome space image of the day from [NASA](https://api.nasa.gov/)
 
 ### Today image : {data["title"]}
@@ -35,6 +35,7 @@ Date : {data["date"]}
 ![]({data["url"]})
 
 <small>Latest update : {update}</small>
-        """)
+                """)
 
-        f.close()
+        except Exception as e:
+            sys.exit(f"An error occurred: {e}")
